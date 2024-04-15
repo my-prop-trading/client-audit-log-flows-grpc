19b1996ebe3c7d4ae0e08f9459be6e9624e3f7d6
Aleksandr Belousov <aleksandr.b@simple.app>
2024-04-15 17:03:33 +0300

    PROP2024-284

--- build.rs
+++ build.rs
@@ -1,4 +1,4 @@
 fn main() {
-    let url = "https://raw.githubusercontent.com/my-prop-trading/proto-files/main/proto/";
+    let url = "https://raw.githubusercontent.com/my-prop-trading/proto-files/PROP2024-284/proto/";
     ci_utils::sync_and_build_proto_file(url, "ClientAuditLogsGrpcService.proto");
 }
